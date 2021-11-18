933fbf16ad541a7d294256dc44bc43b6080d792e
Арзуманов Михаил <marzumanov@softrust.ru>
2021-11-18 14:32:46 +0300

    Ellipse class was formed

--- OpenGL_test.cpp
+++ OpenGL_test.cpp
@@ -9,6 +9,7 @@ using namespace std;
 int rx = 138, ry = 138;
 int xCenter = 250, yCenter = 250;
 
+
 struct point {
     float x, y;
 };
@@ -49,102 +50,92 @@ public:
 
 class Square : public Rectangle {
 public:
-    Square(point c_, float r_, Color color_) :Rectangle(c_, { r_,r_ }, color_) {}
+    Square(point c_, float r_, Color color_) :Rectangle(c_, {r_,r_}, color_) {}
 };
 
 class Ellipse : public Drawable {
 protected:
     point c;
     point r;
+    Color color;
 public:
+    Ellipse(point c_, point r_, Color color_) {
+        c = c_; r = r_; color = color_;
+    }
     void draw() {
+        glBegin(GL_POINTS);
+            float rx = r.x, ry = r.y, xCenter = c.x, yCenter = c.y;
 
+            float p1 = ry * ry - (rx * rx) * ry + (rx * rx) * (0.25f);
+
+            float x = 0;
+            float y = ry;
+
+            float dx = 2 * (ry * ry) * x;
+            float dy = 2 * (rx * rx) * y;
+            color.set_to_env();
+            while (dx < dy) {
+                glVertex2f(xCenter + x, yCenter + y);
+                glVertex2f(xCenter - x, yCenter + y);
+                glVertex2f(xCenter + x, yCenter - y);
+                glVertex2f(xCenter - x, yCenter - y);
+                if (p1 < 0) {
+                    x = x + 1;
+                    dx = 2 * (ry * ry) * x;
+                    p1 += dx + (ry * ry);
+                }
+                else {
+                    x = x + 1;
+                    y = y - 1;
+                    dx = 2 * (ry * ry) * x;
+                    dy = 2 * (rx * rx) * y;
+                    p1 = p1 + dx - dy + (ry * ry);
+                }
+            }
+
+            float p2 = (ry * ry) * (x + 0.5f) * (x + 0.5f) + (rx * rx) * (y - 1) * (y - 1) - (rx * rx) * (ry * ry);
+
+            while (y >= 0) {
+                glVertex2f(xCenter + x, yCenter + y);
+                glVertex2f(xCenter - x, yCenter + y);
+                glVertex2f(xCenter + x, yCenter - y);
+                glVertex2f(xCenter - x, yCenter - y);
+                if (p2 > 0) {
+                    x = x;
+                    y = y - 1;
+                    dy = 2 * (rx * rx) * y;
+                    p2 = p2 - dy + (rx * rx);
+                }
+                else {
+                    x = x + 1;
+                    y = y - 1;
+                    dy = dy - 2 * (rx * rx);
+                    dx = dx + 2 * (ry * ry);
+                    p2 = p2 + dx -
+                        dy + (rx * rx);
+                }
+            }
+        glEnd();
     }
 };
 
-void setPixel(const GLint x, const GLint y)
-{
-    glBegin(GL_POINTS);
-    glVertex2i(x, y);
-    glEnd();
-}
-
-void ellipseMidPoint(float rx, float ry, float xCenter, float yCenter)
-{
-
-    float x = 0;
-    float y = ry;
-    float p1 = ry * ry - (rx * rx) * ry + (rx * rx) * (0.25f);
-    float dx = 2 * (ry * ry) * x;
-    float dy = 2 * (rx * rx) * y;
-    glColor3ub(rand() % 255, rand() % 255, rand() % 255);
-    while (dx < dy)
-    {
-        cout << x << " " << y << " " << dx << " " << dy << " " << p1 << "\n";
-        setPixel(xCenter + x, yCenter + y);
-        setPixel(xCenter - x, yCenter + y);
-        setPixel(xCenter + x, yCenter - y);
-        setPixel(xCenter - x, yCenter - y);
-        if (p1 < 0)
-        {
-            x = x + 1;
-            dx = 2 * (ry * ry) * x;
-            p1 += dx + (ry * ry);
-        }
-        else
-        {
-            x = x + 1;
-            y = y - 1;
-            dx = 2 * (ry * ry) * x;
-            dy = 2 * (rx * rx) * y;
-            p1 = p1 + dx - dy + (ry * ry);
-        }
-    }
-
-    float p2 = (ry * ry) * (x + 0.5f) * (x + 0.5f) + (rx * rx) * (y - 1) * (y - 1) - (rx * rx) * (ry * ry);
-    glColor3ub(rand() % 255, rand() % 255, rand() % 255);
-    while (y > 0)
-    {
-        setPixel(xCenter + x, yCenter + y);
-        setPixel(xCenter - x, yCenter + y);
-        setPixel(xCenter + x, yCenter - y);
-        setPixel(xCenter - x, yCenter - y);
-        if (p2 > 0)
-        {
-            x = x;
-            y = y - 1;
-            dy = 2 * (rx * rx) * y;
-            p2 = p2 - dy + (rx * rx);
-        }
-        else
-        {
-            x = x + 1;
-            y = y - 1;
-            dy = dy - 2 * (rx * rx);
-            dx = dx + 2 * (ry * ry);
-            p2 = p2 + dx -
-                dy + (rx * rx);
-        }
-    }
-    glFlush();
-}
-
 std::vector<Drawable*> drawables;
 void init_drawables() {
-    drawables.push_back(new Square({ 39.f, 39.f }, 38.f, Color{ 255,255,255 }));
-    drawables.push_back(new Square({ 325.f,313.f }, 40.f, Color{ 255,255,255 }));
+    for (float x = 138.f; x <= 249.f; x += 1.f)
+        drawables.push_back(new Ellipse({ x,250.f }, { 138.f,138.f }, Color{255,128,128}));
 }
 
-void display()
-{
+void display(){
     glClear(GL_COLOR_BUFFER_BIT);
     glPointSize(1.0);
-    ellipseMidPoint(rx, ry, xCenter, yCenter);
+    int start = clock();
     for (auto drawable : drawables)
         drawable->draw();
+    int end = clock();
+    cout << end - start <<"\n";
+    glFlush();
 }
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv){
     init_drawables();
 
     glutInit(&argc, argv);
